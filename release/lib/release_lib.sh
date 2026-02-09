bd6f147578df0d47ad64b07e859341d6b8353133
Fabio Erculiani <m@lxnay.me>
2026-02-09 17:02:20 +0100

    Do not check if /opt is empty anymore. /opt was (sadly) moved to /usr/opt.

--- release/lib/release_lib.sh
+++ release/lib/release_lib.sh
@@ -437,8 +437,6 @@ release_lib.post_clean_qa_checks() {
 
     echo "Post-clean QA Checks ..."
 
-    qa_lib.check_opt_emptiness "${imagedir}"
-
     echo "Listing the top 20 largest packages:"
     chroot "${imagedir}" \
         equery size '*' | sed 's/\(.*\):.*(\(.*\))$/\2 \1/' \
