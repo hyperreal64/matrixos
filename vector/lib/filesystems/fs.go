e823940ccc193a5dc3fe7338ca452f6554685aaa
Fabio Erculiani <m@lxnay.me>
2026-02-11 10:24:48 +0100

    Complete initial porting of fs_lib.sh to fs.go and fs_test.go

---
+++ vector/lib/filesystems/fs.go
@@ -0,0 +1,747 @@
+package filesystems
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
+)
+
+var (
+	execCommand     = exec.Command
+	devMapperPrefix = "/dev/mapper"
+)
+
+// DevicesSettle waits for udev events to settle.
+func DevicesSettle() {
+	execCommand("udevadm", "settle").Run()
+}
+
+// FlushBlockDeviceBuffers flushes the buffers of a block device.
+func FlushBlockDeviceBuffers(devPath string) error {
+	if devPath == "" {
+		return fmt.Errorf("missing devPath parameter")
+	}
+	return execCommand("blockdev", "--flushbufs", devPath).Run()
+}
+
+// GetLuksRootfsDevicePath returns the device path for a given LUKS name.
+func GetLuksRootfsDevicePath(luksName string) (string, error) {
+	if luksName == "" {
+		return "", fmt.Errorf("missing luksName parameter")
+	}
+	return filepath.Join(devMapperPrefix, luksName), nil
+}
+
+// DeviceUUID returns the UUID of a given device path.
+func DeviceUUID(devPath string) (string, error) {
+	if devPath == "" {
+		return "", fmt.Errorf("missing argument devpath")
+	}
+	cmd := execCommand("blkid", "-s", "UUID", "-o", "value", devPath)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	trimmedOut := strings.TrimSpace(string(out))
+	if trimmedOut == "" {
+		return "", fmt.Errorf("could not find UUID for %s", devPath)
+	}
+	return trimmedOut, nil
+}
+
+// DevicePartUUID returns the PARTUUID of a given device path.
+func DevicePartUUID(devPath string) (string, error) {
+	if devPath == "" {
+		return "", fmt.Errorf("missing argument devpath")
+	}
+	cmd := execCommand("blkid", "-s", "PARTUUID", "-o", "value", devPath)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	trimmedOut := strings.TrimSpace(string(out))
+	if trimmedOut == "" {
+		return "", fmt.Errorf("could not find PARTUUID for %s", devPath)
+	}
+	return trimmedOut, nil
+}
+
+// MountpointToDevice returns the device path for a given mountpoint.
+func MountpointToDevice(mnt string) (string, error) {
+	if mnt == "" {
+		return "", fmt.Errorf("missing mnt parameter")
+	}
+
+	cmd := execCommand("findmnt", "-no", "SOURCES", mnt)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	trimmedOut := strings.TrimSpace(string(out))
+	if trimmedOut == "" {
+		return "", fmt.Errorf("no device found for mountpoint %s", mnt)
+	}
+	return strings.Split(trimmedOut, "\n")[0], nil
+}
+
+// CleanupMounts unmounts a list of mounts in reverse order.
+func CleanupMounts(mounts []string) {
+	DevicesSettle()
+	for i := len(mounts) - 1; i >= 0; i-- {
+		mnt := mounts[i]
+		out, _ := execCommand("findmnt", "-n", mnt).Output()
+		if len(out) == 0 {
+			continue
+		}
+		log.Printf("Unmounting %s ...", mnt)
+		if err := execCommand("umount", mnt).Run(); err != nil {
+			FlushBlockDeviceBuffers(mnt)
+			log.Printf("Unable to umount %s", mnt)
+			out, _ := execCommand("findmnt", mnt).CombinedOutput()
+			log.Println(string(out))
+			continue
+		}
+	}
+	DevicesSettle()
+}
+
+// CleanupCryptsetupDevices closes a list of cryptsetup devices.
+func CleanupCryptsetupDevices(devices []string) {
+	DevicesSettle()
+	for _, cd := range devices {
+		cdpath, err := GetLuksRootfsDevicePath(cd)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if _, err := os.Stat(cdpath); os.IsNotExist(err) {
+			continue
+		}
+
+		log.Printf("Closing LUKS device: %s ...", cd)
+		FlushBlockDeviceBuffers(cdpath)
+		if err := execCommand("cryptsetup", "close", cd).Run(); err != nil {
+			log.Printf("Unable to cryptsetup close %s", cdpath)
+			out, _ := execCommand("findmnt", cdpath).CombinedOutput()
+			log.Println(string(out))
+			continue
+		}
+	}
+	DevicesSettle()
+}
+
+// CleanupLoopDevices detaches a list of loop devices.
+func CleanupLoopDevices(devices []string) {
+	DevicesSettle()
+	for _, ld := range devices {
+		if _, err := os.Stat(ld); os.IsNotExist(err) {
+			continue
+		}
+		out, _ := execCommand("losetup", "--raw", "-l", "-O", "BACK-FILE", ld).Output()
+		if len(strings.TrimSpace(string(out))) == 0 {
+			continue
+		}
+
+		log.Printf("Cleaning loop device %s ...", ld)
+
+		if err := execCommand("losetup", "-d", ld).Run(); err != nil {
+			FlushBlockDeviceBuffers(ld)
+			log.Printf("Unable to close loop device %s", ld)
+			out, _ := execCommand("findmnt", ld).CombinedOutput()
+			log.Println(string(out))
+			continue
+		}
+	}
+	DevicesSettle()
+}
+
+// ListSubmounts returns a list of submounts for a given mountpoint.
+func ListSubmounts(mnt string) ([]string, error) {
+	if mnt == "" {
+		return nil, fmt.Errorf("missing argument")
+	}
+	cmd := execCommand("findmnt", "-rn", "-o", "TARGET", "--submounts", "--target", mnt)
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var submounts []string
+	for line := range strings.SplitSeq(string(out), "\n") {
+		if strings.HasPrefix(line, mnt) {
+			submounts = append(submounts, line)
+		}
+	}
+	return submounts, nil
+}
+
+// CheckDirNotFsRoot checks if a directory is the root of the filesystem.
+func CheckDirNotFsRoot(mnt string) error {
+	if mnt == "" {
+		return fmt.Errorf("missing mnt parameter")
+	}
+
+	rootStat, err := os.Stat("/")
+	if err != nil {
+		return err
+	}
+	mntStat, err := os.Stat(mnt)
+	if err != nil {
+		return err
+	}
+
+	if os.SameFile(rootStat, mntStat) {
+		return fmt.Errorf("CRITICAL ERROR: %s IS MAPPED TO HOST ROOT. ABORTING", mnt)
+	}
+	return nil
+}
+
+var slaveMounts = []string{
+	"/dev",
+	"/dev/pts",
+	"/sys",
+}
+
+// SetupCommonRootfsMounts sets up common rootfs mounts.
+func SetupCommonRootfsMounts(mnt string) ([]string, error) {
+	if mnt == "" {
+		return nil, fmt.Errorf("missing mnt parameter")
+	}
+
+	if _, err := os.Stat(mnt); os.IsNotExist(err) {
+		return nil, fmt.Errorf("%s does not exist", mnt)
+	}
+	if err := CheckDirNotFsRoot(mnt); err != nil {
+		return nil, err
+	}
+
+	var mountsList []string
+	for _, d := range slaveMounts {
+		dst := filepath.Join(mnt, d)
+		if err := os.MkdirAll(dst, 0755); err != nil {
+			return nil, err
+		}
+		if err := execCommand("mount", "-v", "--bind", d, dst).Run(); err != nil {
+			return nil, err
+		}
+		mountsList = append(mountsList, dst)
+		if err := execCommand("mount", "-v", "--make-slave", dst).Run(); err != nil {
+			return nil, err
+		}
+	}
+
+	chrootDevShm := filepath.Join(mnt, "dev", "shm")
+	if err := os.MkdirAll(chrootDevShm, 0755); err != nil {
+		return nil, err
+	}
+	err := execCommand(
+		"mount", "-v", "-t", "tmpfs", "devshm", chrootDevShm,
+		"-o", "rw,mode=1777,nosuid,nodev",
+	).Run()
+	if err != nil {
+		return nil, err
+	}
+	mountsList = append(mountsList, chrootDevShm)
+
+	chrootProc := filepath.Join(mnt, "proc")
+	if err := os.MkdirAll(chrootProc, 0755); err != nil {
+		return nil, err
+	}
+	err = execCommand("mount", "-t", "proc", "proc", chrootProc).Run()
+	if err != nil {
+		return nil, err
+	}
+	mountsList = append(mountsList, chrootProc)
+
+	runLock := filepath.Join(mnt, "run", "lock")
+	if err := os.MkdirAll(runLock, 0755); err != nil {
+		return nil, err
+	}
+	err = execCommand(
+		"mount", "-v", "-t", "tmpfs", "none", runLock,
+		"-o", "rw,nosuid,nodev,noexec,relatime,size=5120k",
+	).Run()
+	if err != nil {
+		return nil, err
+	}
+	mountsList = append(mountsList, runLock)
+
+	return mountsList, nil
+}
+
+// UnsetupCommonRootfsMounts unsets common rootfs mounts.
+func UnsetupCommonRootfsMounts(mnt string) error {
+	if mnt == "" {
+		return fmt.Errorf("missing mnt parameter")
+	}
+
+	if _, err := os.Stat(mnt); os.IsNotExist(err) {
+		return fmt.Errorf("%s does not exist", mnt)
+	}
+	if err := CheckDirNotFsRoot(mnt); err != nil {
+		return err
+	}
+
+	var mounts []string
+	for _, d := range slaveMounts {
+		mounts = append(mounts, filepath.Join(mnt, d))
+	}
+	mounts = append(mounts,
+		filepath.Join(mnt, "dev", "shm"),
+		filepath.Join(mnt, "proc"),
+		filepath.Join(mnt, "run", "lock"),
+	)
+	CleanupMounts(mounts)
+	return nil
+}
+
+// BindMount binds a source directory to a destination directory.
+func BindMount(src, dst string) (string, error) {
+	if src == "" {
+		return "", fmt.Errorf("missing src parameter")
+	}
+	if dst == "" {
+		return "", fmt.Errorf("missing dst parameter")
+	}
+
+	if _, err := os.Stat(src); os.IsNotExist(err) {
+		return "", fmt.Errorf("%s does not exist", src)
+	}
+	if _, err := os.Stat(dst); os.IsNotExist(err) {
+		return "", fmt.Errorf("%s does not exist", dst)
+	}
+
+	if err := CheckDirNotFsRoot(src); err != nil {
+		return "", err
+	}
+	if err := CheckDirNotFsRoot(dst); err != nil {
+		return "", err
+	}
+
+	// log.Printf("Binding %s to %s", src, dst)
+	if err := execCommand("mount", "-v", "--bind", src, dst).Run(); err != nil {
+		return "", err
+	}
+	if err := execCommand("mount", "-v", "--make-slave", dst).Run(); err != nil {
+		return "", err
+	}
+	return dst, nil
+}
+
+// BindUmount unmounts a bind mount.
+func BindUmount(mnt string) error {
+	if mnt == "" {
+		return fmt.Errorf("missing mnt parameter")
+	}
+	if _, err := os.Stat(mnt); os.IsNotExist(err) {
+		return fmt.Errorf("%s does not exist", mnt)
+	}
+	if err := CheckDirNotFsRoot(mnt); err != nil {
+		return err
+	}
+	CleanupMounts([]string{mnt})
+	return nil
+}
+
+// BindMountDistdir binds the distfiles directory.
+func BindMountDistdir(distfilesDir, rootfs string) (string, error) {
+	if distfilesDir == "" {
+		return "", fmt.Errorf("missing parameter distfilesDir")
+	}
+	if rootfs == "" {
+		return "", fmt.Errorf("missing rootfs parameter")
+	}
+
+	if _, err := os.Stat(distfilesDir); os.IsNotExist(err) {
+		return "", fmt.Errorf("%s does not exist", distfilesDir)
+	}
+	if _, err := os.Stat(rootfs); os.IsNotExist(err) {
+		return "", fmt.Errorf("%s does not exist", rootfs)
+	}
+
+	dstDir := filepath.Join(rootfs, "var", "cache", "distfiles")
+	if err := os.MkdirAll(dstDir, 0755); err != nil {
+		return "", err
+	}
+	return BindMount(distfilesDir, dstDir)
+}
+
+// BindUmountDistdir unmounts the distfiles directory.
+func BindUmountDistdir(rootfs string) error {
+	if rootfs == "" {
+		return fmt.Errorf("missing rootfs parameter")
+	}
+	if _, err := os.Stat(rootfs); os.IsNotExist(err) {
+		return fmt.Errorf("%s does not exist", rootfs)
+	}
+	dstDir := filepath.Join(rootfs, "var", "cache", "distfiles")
+	return BindUmount(dstDir)
+}
+
+// BindMountBinpkgs binds the binpkgs directory.
+func BindMountBinpkgs(binpkgsDir, rootfs string) (string, error) {
+	if binpkgsDir == "" {
+		return "", fmt.Errorf("missing parameter binpkgsDir")
+	}
+	if rootfs == "" {
+		return "", fmt.Errorf("missing rootfs parameter")
+	}
+
+	if _, err := os.Stat(binpkgsDir); os.IsNotExist(err) {
+		return "", fmt.Errorf("%s does not exist", binpkgsDir)
+	}
+	if _, err := os.Stat(rootfs); os.IsNotExist(err) {
+		return "", fmt.Errorf("%s does not exist", rootfs)
+	}
+
+	dstDir := filepath.Join(rootfs, "var", "cache", "binpkgs")
+	if err := os.MkdirAll(dstDir, 0755); err != nil {
+		return "", err
+	}
+	return BindMount(binpkgsDir, dstDir)
+}
+
+// BindUmountBinpkgs unmounts the binpkgs directory.
+func BindUmountBinpkgs(rootfs string) error {
+	if rootfs == "" {
+		return fmt.Errorf("missing rootfs parameter")
+	}
+	if _, err := os.Stat(rootfs); os.IsNotExist(err) {
+		return fmt.Errorf("%s does not exist", rootfs)
+	}
+	dstDir := filepath.Join(rootfs, "var", "cache", "binpkgs")
+	return BindUmount(dstDir)
+}
+
+// CheckFsCapabilitySupport checks if the filesystem has capability support.
+var CheckFsCapabilitySupport = checkFsCapabilitySupport
+
+func checkFsCapabilitySupport(testDir string) (bool, error) {
+	if testDir == "" {
+		return false, fmt.Errorf("missing parameter testDir")
+	}
+
+	tmpBin, err := os.CreateTemp(testDir, ".cap_test.*.bin")
+	if err != nil {
+		return false, err
+	}
+	tmpBin.Close()
+	defer os.Remove(tmpBin.Name())
+
+	if err := execCommand("setcap", "cap_net_raw+ep", tmpBin.Name()).Run(); err != nil {
+		log.Println("WARNING: System/FS does not allow setting capabilities.")
+		return false, nil
+	}
+
+	tmpCopy := tmpBin.Name() + ".copy"
+	defer os.Remove(tmpCopy)
+
+	if err := execCommand("cp", "-a", tmpBin.Name(), tmpCopy).Run(); err != nil {
+		return false, err
+	}
+
+	out, err := execCommand("getcap", tmpCopy).Output()
+	if err != nil {
+		// getcap might fail if no caps? No, it just prints nothing usually.
+		return false, err
+	}
+
+	outStr := string(out)
+	// Flexible check for "cap_net_raw+ep" or "cap_net_raw=ep"
+	if strings.Contains(outStr, "cap_net_raw+ep") || strings.Contains(outStr, "cap_net_raw=ep") {
+		return true, nil
+	}
+	return false, nil
+}
+
+// CheckHardlinkPreservation verifies that hardlinks are preserved between source and destination.
+func CheckHardlinkPreservation(src, dst string) error {
+	if src == "" || dst == "" {
+		return fmt.Errorf("missing parameter (src: %s, dst: %s)", src, dst)
+	}
+	log.Printf("Checking hardlink preservation from %s to %s...", src, dst)
+
+	// find files with >1 links, print inode and path.
+	cmd := execCommand("find", src, "-type", "f", "-links", "+1", "-printf", "%i %p\n")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	inodes := make(map[string][]string)
+	var file1Src, file2Src string
+	foundPair := false
+
+	// Stream output to avoid reading everything if not needed?
+	// Actually we need to Find TWO files with SAME inode.
+	// We scan until we fill an inode entry with 2 paths.
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, " ", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		ino, path := parts[0], parts[1]
+		inodes[ino] = append(inodes[ino], path)
+		if len(inodes[ino]) >= 2 {
+			file1Src = inodes[ino][0]
+			file2Src = inodes[ino][1]
+			foundPair = true
+			break
+		}
+	}
+	// Kill the find process if it's still running, we got what we need.
+	cmd.Process.Signal(os.Interrupt)
+	cmd.Wait() // clean up
+
+	if !foundPair {
+		log.Println("WARNING: no hardlinked file pairs found in source. Cannot verify.")
+		return nil
+	}
+
+	relPath1, err := filepath.Rel(src, file1Src)
+	if err != nil {
+		return err
+	}
+	relPath2, err := filepath.Rel(src, file2Src)
+	if err != nil {
+		return err
+	}
+
+	file1Dst := filepath.Join(dst, relPath1)
+	file2Dst := filepath.Join(dst, relPath2)
+
+	info1, err := os.Stat(file1Dst)
+	if err != nil {
+		return err
+	}
+	info2, err := os.Stat(file2Dst)
+	if err != nil {
+		return err
+	}
+
+	stat1, ok1 := info1.Sys().(*syscall.Stat_t)
+	stat2, ok2 := info2.Sys().(*syscall.Stat_t)
+
+	if !ok1 || !ok2 {
+		return fmt.Errorf("could not get inode info")
+	}
+
+	if stat1.Ino != stat2.Ino {
+		return fmt.Errorf(
+			"CRITICAL: hardlinks BROKEN! Files were duplicated.\n  File 1: %s (inode: %d)\n  File 2: %s (inode: %d)",
+			file1Dst, stat1.Ino, file2Dst, stat2.Ino,
+		)
+	}
+
+	log.Printf("SUCCESS: hardlinks preserved (Inode: %d).", stat1.Ino)
+	return nil
+}
+
+// CheckDirIsRoot checks if a directory is the root of the filesystem and exits if it is.
+func CheckDirIsRoot(chrootDir string) error {
+	if chrootDir == "" {
+		return fmt.Errorf("missing chrootDir parameter")
+	}
+	rootStat, err := os.Stat("/")
+	if err != nil {
+		return err
+	}
+	chrootStat, err := os.Stat(chrootDir)
+	if err != nil {
+		return err
+	}
+	if os.SameFile(rootStat, chrootStat) {
+		return fmt.Errorf("CRITICAL ERROR: %s IS MAPPED TO HOST ROOT. ABORTING", chrootDir)
+	}
+	return nil
+}
+
+// CheckDirsSameFilesystem checks if two directories are on the same filesystem.
+func CheckDirsSameFilesystem(src, dst string) (bool, error) {
+	if src == "" || dst == "" {
+		return false, fmt.Errorf("missing parameters src or dst")
+	}
+	srcStat, err := os.Stat(src)
+	if err != nil {
+		return false, err
+	}
+	dstStat, err := os.Stat(dst)
+	if err != nil {
+		return false, err
+	}
+	return srcStat.Sys().(*syscall.Stat_t).Dev == dstStat.Sys().(*syscall.Stat_t).Dev, nil
+}
+
+// CheckActiveMounts checks for active mounts under a given directory.
+func CheckActiveMounts(chrootDir string) error {
+	if chrootDir == "" {
+		return fmt.Errorf("missing chrootDir parameter")
+	}
+	out, _ /* ignore any errors */ := execCommand(
+		"findmnt", "-rn", "-o", "TARGET", "--submounts",
+		"--target", chrootDir,
+	).Output()
+
+	// we do not expect weird chars in mount points.
+	if len(out) == 0 {
+		return nil
+	}
+
+	var foundMounts []string
+	// Check if the output actually contains paths starting with chrootDir
+	for bl := range bytes.SplitSeq(out, []byte("\n")) {
+		if len(bl) == 0 {
+			continue
+		}
+
+		bl = bytes.ToValidUTF8(bl, []byte("?"))
+		line := string(bl)
+		if strings.HasPrefix(line, chrootDir) {
+			foundMounts = append(foundMounts, line)
+		}
+	}
+
+	if len(foundMounts) > 0 {
+		return fmt.Errorf(
+			"cannot operate sync to %s. Active mounts detected:\n- %s\nPlease umount manually.",
+			chrootDir, strings.Join(foundMounts, "\n- "),
+		)
+	}
+
+	return nil
+}
+
+// CpReflinkCopyAllowed checks if a reflink copy is allowed.
+func CpReflinkCopyAllowed(src, dst string, useCpFlag bool) (bool, error) {
+	if src == "" || dst == "" {
+		return false, fmt.Errorf("missing parameters (src: %s, dst: %s)", src, dst)
+	}
+	if !useCpFlag || src == "/" {
+		return false, nil
+	}
+	sameFs, err := CheckDirsSameFilesystem(src, dst)
+	if err != nil {
+		return false, err
+	}
+	if !sameFs {
+		return false, nil
+	}
+	srcCap, err := CheckFsCapabilitySupport(src)
+	if err != nil {
+		return false, err
+	}
+	dstCap, err := CheckFsCapabilitySupport(dst)
+	if err != nil {
+		return false, err
+	}
+	return srcCap && dstCap, nil
+}
+
+// CreateTempDir creates a temporary directory.
+func CreateTempDir(parentDir, prefix string) (string, error) {
+	if parentDir == "" {
+		return "", fmt.Errorf("missing parentDir parameter")
+	}
+	if prefix == "" {
+		prefix = "tmp"
+	}
+	// os.MkdirTemp is the replacement for ioutil.TempDir
+	tempDir, err := os.MkdirTemp(parentDir, prefix)
+	if err != nil {
+		return "", fmt.Errorf("failed to create temporary directory: %w", err)
+	}
+	return tempDir, nil
+}
+
+// CreateTempFile creates a temporary file.
+func CreateTempFile(parentDir, prefix string) (*os.File, error) {
+	if parentDir == "" {
+		return nil, fmt.Errorf("missing parentDir parameter")
+	}
+	if prefix == "" {
+		prefix = "tmp"
+	}
+	// os.CreateTemp is the replacement for ioutil.TempFile
+	tempFile, err := os.CreateTemp(parentDir, prefix)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temporary file: %w", err)
+	}
+	return tempFile, nil
+}
+
+// RemoveFileWithGlob removes files matching a glob pattern.
+func RemoveFileWithGlob(target string) error {
+	matches, err := filepath.Glob(target)
+	if err != nil {
+		return err
+	}
+	for _, match := range matches {
+		err := os.Remove(match)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// RemoveDir removes a directory.
+func RemoveDir(target string) error {
+	if _, err := os.Stat(target); os.IsNotExist(err) {
+		log.Printf("Removing: %s does not exist\n", target)
+		return nil
+	}
+	log.Printf("Removing %s\n", target)
+	return os.RemoveAll(target)
+}
+
+// EmptyDir empties a directory.
+func EmptyDir(target string) error {
+	if _, err := os.Stat(target); os.IsNotExist(err) {
+		log.Printf("Emptying: %s does not exist\n", target)
+		return nil
+	}
+	log.Printf("Emptying directory %s\n", target)
+
+	entries, err := os.ReadDir(target)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		path := filepath.Join(target, entry.Name())
+		if err := os.RemoveAll(path); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// DirEmpty checks if a directory is empty.
+func DirEmpty(dir string) (bool, error) {
+	f, err := os.Open(dir)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.ReadDir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return false, nil
+}
