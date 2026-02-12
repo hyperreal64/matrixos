e98dd0355eb3a9a86c2cd6effd9f9b84ef2f0493
Fabio Erculiani <m@lxnay.me>
2026-02-12 21:50:49 +0100

    Refactor build.sh to improve help argument handling and streamline main function logic

--- dev/build.sh
+++ dev/build.sh
@@ -21,21 +21,48 @@ export MATRIXOS_DEV_DIR
 source "${MATRIXOS_DEV_DIR}/lib/ostree_lib.sh"
 source "${MATRIXOS_DEV_DIR}/lib/qa_lib.sh"
 
+_is_help_arg() {
+    local arg="${1:-}"
+    case "${arg}" in
+        -h|--help)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
 
-main() {
-    ostree_lib.setup_environment
-    qa_lib.root_privs
+_is_help_in_args() {
+    for arg in "${@}"; do
+        if _is_help_arg "${arg}"; then
+            return 0
+        fi
+    done
+    return 1
+}
 
+_print_build_warning() {
     echo "ATTENTION PLEASE"
     echo "Using Git repo: ${MATRIXOS_GIT_REPO} (from conf/matrixos.conf)"
     echo "If you want to make changes to the build configs, it's preferred to fork the official repo"
     echo "and > edit conf/matrixos.conf GitRepo parameter, setting the URL to your ${MATRIXOS_OSNAME}.git repo fork."
+    echo
     echo "Alternatively, use UseLocalGitRepoInsideChroot and optionally also DeleteDotGitFromGitRepo conf/matrixos.conf"
     echo "settings, to do a local clone of the git repository inside the chroot for bootstrapping."
     echo "In both cases, the repo will be cloned inside seed chroots via git clone. This means that all uncommitted"
-    echo "changes will NOT be picked up by the build process. The build will start in 10 seconds ..."
+    echo "changes will NOT be picked up by the build process. The build will start in 5 seconds ..."
     echo
-    sleep 10
+    sleep 5
+}
+
+main() {
+
+    if ! _is_help_in_args "${@}"; then
+        qa_lib.root_privs
+        _print_build_warning
+        ostree_lib.setup_environment
+    fi
 
     local private_git_url="${MATRIXOS_PRIVATE_EXAMPLE_GIT_REPO}"
     local private_repo_path="${MATRIXOS_PRIVATE_GIT_REPO_PATH}"
@@ -50,10 +77,10 @@ main() {
         echo "${private_repo_path} must be a git repo" >&2
         return 1
     else
-        echo "Updating ${private_repo_path} ..."
         (
             cd "${private_repo_path}"
             if [ ! -e .built ] && [ ! -e "${MATRIXOS_SECUREBOOT_KEY_PATH}" ]; then
+                echo "Updating ${private_repo_path} ..."
                 ./make.sh
             fi
         )
