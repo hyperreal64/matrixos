97ac5dfab53cba4a930773a9a9196fbd96e5506a
Fabio Erculiani <m@lxnay.me>
2026-02-18 16:21:37 +0100

    Remove smoke test disablement message in smoke.sh for build server transition

--- vector/tests/smoke.sh
+++ vector/tests/smoke.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -eu
 
-# TODO: remove this once moved to the new build server.
-echo "SMOKE TEST CURRENTLY DISABLED, PENDING MOVE TO NEW SERVER" >&2
-exit 0
-
 IMAGE_PATH="${1}"
 shift
 
