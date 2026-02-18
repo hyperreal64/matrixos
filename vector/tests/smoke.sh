8bcbe8ef50a76f6494a8823d7f4ddec01510d196
Fabio Erculiani <m@lxnay.me>
2026-02-18 10:48:50 +0100

    Remove execution logic from smoke.sh for build server transition

--- vector/tests/smoke.sh
+++ vector/tests/smoke.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eu
 
+# TODO: remove this once moved to the new build server.
+echo "SMOKE TEST CURRENTLY DISABLED, PENDING MOVE TO NEW SERVER" >&2
+exit 0
+
 IMAGE_PATH="${1}"
 shift
 
